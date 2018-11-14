c883b2524bb5ddc8b65dc9854c6c1cf81c8ebdfb
温瑜玲 <wenyuling10@qq.com>
2018-11-14 11:33:49 +0800

    订单模型

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    protected $fillable = ['amount', 'price', 'rating', 'review', 'reviewed_at'];
+    protected $dates = ['reviewed_at'];
+    public $timestamps = false; //false 没有created_at 和 updated_at 两个时间戳字段
+
+    /**
+     * 关联商品表
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * 关联商品sku表
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function productSku()
+    {
+        return $this->belongsTo(ProductSku::class);
+    }
+
+    /**
+     * 关联订单表
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
