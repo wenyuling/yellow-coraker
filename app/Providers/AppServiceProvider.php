035fb4f8ed2b5ceb39f1ebf17ba55d2cc458129a
温瑜玲 <wenyuling10@qq.com>
2018-11-20 16:52:41 +0800

    引入支付库

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,15 @@
 
 namespace App\Providers;
 
+use Monolog\Logger;
+use Yansongda\Pay\Pay;
 use Illuminate\Support\ServiceProvider;
 
+/**
+ * 支付操作类实例注入到容器中
+ * Class AppServiceProvider
+ * @package App\Providers
+ */
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -23,6 +30,31 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        // 往服务容器中注入一个名为 alipay 的单例对象
+        $this->app->singleton('alipay', function () {
+            $config = config('pay.alipay');
+            // 判断当前项目运行环境是否为线上环境
+            if (app()->environment() !== 'production') {
+                //不是线上环境，则启用开发模式
+                $config['mode']         = 'dev';
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 调用 Yansongda\Pay 来创建一个支付宝支付对象
+            return Pay::alipay($config);
+        });
+
+        $this->app->singleton('wechat_pay', function () {
+            $config = config('pay.wechat');
+            if (app()->environment() !== 'production') {
+                $config['log']['level'] = Logger::DEBUG;
+            } else {
+                $config['log']['level'] = Logger::WARNING;
+            }
+            // 调用 Yansongda\Pay 来创建一个微信支付对象
+            return Pay::wechat($config);
+        });
     }
+
 }
