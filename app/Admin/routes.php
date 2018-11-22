b850b4a95777b21576d09381eec4700eec1d8a93
温瑜玲 <wenyuling10@qq.com>
2018-11-22 11:33:09 +0800

    后台订单发货

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -19,4 +19,5 @@ Route::group([
 
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index'); //订单列表
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show'); //订单详情
+    $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship'); //发货功能
 });
