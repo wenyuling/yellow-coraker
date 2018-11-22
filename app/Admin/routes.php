a701c7dc9bb740a706f9896e2c1ecbceb1936b86
温瑜玲 <wenyuling10@qq.com>
2018-11-22 15:54:14 +0800

    后台拒绝退款

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -20,4 +20,7 @@ Route::group([
     $router->get('orders', 'OrdersController@index')->name('admin.orders.index'); //订单列表
     $router->get('orders/{order}', 'OrdersController@show')->name('admin.orders.show'); //订单详情
     $router->post('orders/{order}/ship', 'OrdersController@ship')->name('admin.orders.ship'); //发货功能
+
+    $router->post('orders/{order}/refund', 'OrdersController@handleRefund')->name('admin.orders.handle_refund'); //处理退款
+
 });
